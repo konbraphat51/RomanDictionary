1ff62c396a9dd3c8d79699ed3a9454ad0cb88339
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-08-08 01:53:33 +0700

    fix pathing

--- RomanDictionary/Utils/Consts.py
+++ RomanDictionary/Utils/Consts.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+
 class Consts:
-    datamaker_folder = "DataMaker/"
-    data_folder = "Data/"
+    datamaker_folder = "RomanDictionary/DataMaker/"
+    data_folder = "RomanDictionary/Data/"
+
+library_dir = Path(__file__).parent.parent
+Consts.datamaker_folder = str(library_dir / "DataMaker/") + "\\"
+Consts.data_folder = str(library_dir / "Data/") + "\\"
