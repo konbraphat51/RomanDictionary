2be9846cfa4c6b298d732a1029adb30dbedfa709
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-08-07 23:59:12 +0700

    gradio app

---
+++ app.py
@@ -0,0 +1,10 @@
+import gradio as gr
+from RomanDictionary.Seacher import Searcher
+
+searcher = Searcher()
+
+app = gr.Interface(
+    fn=searcher.search,
+    inputs=["text", gr.Radio(["Eng", "Romanized", "Th"])],
+    outputs="text"
+)
